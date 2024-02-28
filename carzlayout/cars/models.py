2549f66dadb9cb3ad070ebf7ae8c3894abfadeb2
Fritz <grom.alexey@gmail.com>
2024-02-28 23:06:14 +0600

    added KTG property

--- carzlayout/cars/models.py
+++ carzlayout/cars/models.py
@@ -234,6 +234,7 @@ class Car(models.Model):
         PUBLISHED = 1, 'Опубликовано'
 
     title = models.CharField(max_length=255, verbose_name='Название')
+    KTG = models.FloatField(blank=True,null=True,verbose_name='КТГ')
     slug = models.SlugField(max_length=255,unique=True, db_index=True)
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/',default=None,blank=True,null=True,verbose_name='Фоточка')
     content = models.TextField(blank=True,verbose_name="Описание")
