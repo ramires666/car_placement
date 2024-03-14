4e6ef46403e735a63eeae22a5818e68bb309416e
Fritz <grom.alexey@gmail.com>
2024-03-14 22:44:19 +0500

    2 prod

--- carzlayout/carzlayout/settings.py
+++ carzlayout/carzlayout/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6=@lj59t+=()!^)$g%4r55*lj3$40p(998y-jd)lrf(mdy9ys0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
